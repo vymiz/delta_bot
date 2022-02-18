31c5ac7471c353e99da4f4501def2aa88f3b92a9
vymiz <vymiz020777@gmail.com>
2022-02-18 19:14:02 +0300

    0.002

--- bot.py
+++ bot.py
@@ -2,10 +2,16 @@ from addresses import *
 from get_strike_func import *
 from read_file_func import *
 
+pause = 15
+
 delta_call, strike, delta_put, price = read()
 
 delta_call, strike, delta_put, price = get_strike(delta_call, strike, delta_put, price)
 
-print(delta_call, strike, delta_put, price)
 
+while True:
+    delta_call, strike, delta_put, price = read()
+    delta_call, strike, delta_put, price = get_strike(delta_call, strike, delta_put, price)
+    print(delta_call, strike, delta_put, price)
 
+    time.sleep(pause)
