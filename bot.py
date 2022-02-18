d1b80b388df9d3a92a6ef9f3b5ffe1170f8e5bc9
vymiz <vymiz020777@gmail.com>
2022-02-18 20:37:07 +0300

    0.004

--- bot.py
+++ bot.py
@@ -8,11 +8,23 @@ delta_call, strike_first, delta_put, price = read()
 
 delta_call, strike_first, delta_put, price = get_strike(delta_call, strike_first, delta_put, price)
 
+old_pose = delta_call + delta_put
 
 while True:
+    # получаем массивы данных
     delta_call, strike_last, delta_put, price = read()
+
+    # получаем единичные значения
     delta_call, strike_last, delta_put, price = get_strike(delta_call, strike_last, delta_put, price)
-    print(delta_call, strike_first, delta_put, price, end=' ')
-    print(delta_call + delta_put)
+
+    new_pose = delta_call + delta_put
+    trade_pose = new_pose - old_pose
+    old_pose = new_pose
+
+    print('call:%-3d  strike:%-6d  put:%-3d  price:%-6d' % (delta_call, strike_first, delta_put, price), end='  ')
+    print('pose:%-3d' % trade_pose)
 
     time.sleep(pause)
+
+    # print('call:%-3d  strike:%-6d  put:%-3d  price:%-6d' % (delta_call, strike_first, delta_put, price), end='  ')
+    # print('pose:%-3d' % (delta_call + delta_put))
